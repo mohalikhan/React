7e54b3c1f09c587d194ca80ae0143cf903123e54
Mohammad Ali <mohalikhan@hotmail.com>
2019-08-27 12:11:48 -0400

    Updates

---
+++ precache-manifest.03a52c24ba73d22bae69ae790dd4ed54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "816e5751ec15cf04f11ee482996765de",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "20d55c267b091f6c051d",
+    "url": "/React/static/css/main.6f88b6bd.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/React/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "20d55c267b091f6c051d",
+    "url": "/React/static/js/main.924785e5.chunk.js"
+  },
+  {
+    "revision": "ab6fd28d7c2e1eb8fbad",
+    "url": "/React/static/js/runtime~main.2ad8acb2.js"
+  }
+]);
