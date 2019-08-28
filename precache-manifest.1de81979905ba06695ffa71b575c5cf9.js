38035c741b757f3ef6cb8470cf9219e0718851be
Mohammad Ali <mohalikhan@hotmail.com>
2019-08-28 11:45:13 -0400

    Updates

---
+++ precache-manifest.1de81979905ba06695ffa71b575c5cf9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35559eb14400d41b2a3be2f95440e3bc",
+    "url": "/React/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "85c6b69e9ab3b52edf7b",
+    "url": "/React/monsters-rolodex/static/css/main.6f88b6bd.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/React/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "85c6b69e9ab3b52edf7b",
+    "url": "/React/monsters-rolodex/static/js/main.764611f1.chunk.js"
+  },
+  {
+    "revision": "db0aec49a8edf8500453",
+    "url": "/React/monsters-rolodex/static/js/runtime~main.aefcd809.js"
+  }
+]);
